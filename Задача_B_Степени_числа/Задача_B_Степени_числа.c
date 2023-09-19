b755f9fb99ee52706e531009face0ad9ac908651
Андрей Боев <a.boev.it@gmail.com>
2023-09-19 03:29:18 +0300

    Решение задач (3 часть)

--- Задача_B_Степени_числа/Задача_B_Степени_числа.c
+++ Задача_B_Степени_числа/Задача_B_Степени_числа.c
@@ -1,6 +1,15 @@
+#include <stdio.h>
 
 int main(int argc, char* argv[])
 {
-    
+    long long n, res;
+    scanf_s("%lli", &n);
+    res = 1;
+    if (n != 1)
+        while (res < 1000000000)
+        {
+            printf("%lli ", res);
+            res *= n;
+        }
     return 0;
 }

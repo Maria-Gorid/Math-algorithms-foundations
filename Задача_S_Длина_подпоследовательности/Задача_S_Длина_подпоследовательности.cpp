6f63339068b92350a12f5c5bbd33b3bc017700b6
Андрей Боев <a.boev.it@gmail.com>
2023-11-21 13:24:47 +0300

    Обновление алгоритма поиска наибольшей подпоследовательности

--- Задача_S_Длина_подпоследовательности/Задача_S_Длина_подпоследовательности.cpp
+++ Задача_S_Длина_подпоследовательности/Задача_S_Длина_подпоследовательности.cpp
@@ -4,22 +4,28 @@
 
 int main(int argc, char* argv[])
 {
-	int n;
-	std::cin >> n;
-	int m = 0, count = 0, x;
-	std::vector<int> nums;
-	std::vector<int> d(n, -4294967296);
-	for (int i = 0; i < n; i++)
-	{
-		std::cin >> x;
-		nums.push_back(x);
-	}
-	for (int i = 0; i < n; i++)
-	{
-		int pos = std::lower_bound(d.begin(), d.end(), nums[i]) - d.begin();
-		if (d[pos - 1] > nums[i] && nums[i] > d[pos])
-			d[pos] = nums[i];
-	}
-	std::cout << d[n - 1];
-	return 0;
+    int n;
+    std::cin >> n;
+    int m = 0, count = 0, x;
+    std::vector<int> nums;
+    std::vector<int> d(n + 1, 2147483647);
+    for (int i = 1; i <= n; i++)
+    {
+        std::cin >> x;
+        nums.push_back(x);
+    }
+    d[0] = -2147483647;
+    for (int i = 0; i < n; i++)
+    {
+        int pos = (int)(std::upper_bound(d.begin(), d.end(), nums[i],
+            [](double value, int n) { return value <= n;}) - d.begin());
+        if (d[pos - 1] < nums[i] && nums[i] < d[pos])
+        {
+            d[pos] = nums[i];
+            m = std::max(m, pos);
+        }
+
+    }
+    std::cout << m;
+    return 0;
 }
